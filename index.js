452544c1f25f6679235ddbb3d08ad65995af0b75
changtimwu <changtimwu@gmail.com>
2018-04-25 18:22:04 +0800

    basic initilization for loading web3 and RN bundle config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('./globals')
 import { AppRegistry } from 'react-native';
 import App from './App';
 
---
+++ rn-cli.config.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = {
+    extraNodeModules: require('node-libs-browser')
+};
