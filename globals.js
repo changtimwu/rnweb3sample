2860b5b675553d23654d9fcc2f89aa1c7e92f263
changtimwu <changtimwu@gmail.com>
2018-04-25 18:40:59 +0800

    supply btoa

--- globals.js
+++ globals.js
@@ -1,5 +1,6 @@
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+global.btoa = (b)=> Buffer.from(b).toString('base64')
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
     protocol: 'file:',
